8857fff29cee39c7cd3773d72443aa0999d00220
wgh <1127003038@qq.com>
2022-11-08 11:38:01 +0800

    448-1024 comparison

--- main.py
+++ main.py
@@ -5,53 +5,81 @@ import numpy as np
 from utils.load_image import load_image_from_disk, load_image_example
 from scale.pillow_scaler import PillowScaler, Algorithm
 from scale.attack import Attack
-from sewar.full_ref import mse, rmse
 
 
 if __name__ == '__main__':
 
-    src, tar = load_image_from_disk(r'/home/scaling_attack/tiny-imagenet-200/train/0/images',
-                                    r'/home/scaling_attack/tiny-imagenet-200/train/1/images')
-    scaler_1 = PillowScaler(Algorithm.NEAREST, (448, 448), (64, 64))
-    scaler_2 = PillowScaler(Algorithm.NEAREST, (448, 448), (96, 96))
-    scaler_3 = PillowScaler(Algorithm.NEAREST, (448, 448), (114, 114))
-    # scaler_4 = PillowScaler(Algorithm.LANCZOS, (224, 224), (144, 144))
-    sim = []
-    for i in range(3):
-        attack = Attack(src[i], tar[i], [scaler_1, scaler_2, scaler_3])
-        att = attack.attack()
+    plt.figure(dpi=300, figsize=(10, 10))
 
+    for i in range(3):
+        if i == 0:
+            src, tar = load_image_from_disk(r'/home/scaling_attack/cifar/train/0',
+                                            r'/home/scaling_attack/cifar/train/1')
+        elif i == 1:
+            src, tar = load_image_from_disk(r'/home/scaling_attack/pub_60/benign/train/0',
+                                            r'/home/scaling_attack/pub_60/benign/train/1')
+        else:
+            src, tar = load_image_from_disk(r'/home/scaling_attack/tiny-imagenet-200/train/0/images',
+                                            r'/home/scaling_attack/tiny-imagenet-200/train/1/images')
+
+        scaler_1 = PillowScaler(Algorithm.NEAREST, (448, 448), (64, 64))
+        scaler_2 = PillowScaler(Algorithm.NEAREST, (448, 448), (96, 96))
+        scaler_3 = PillowScaler(Algorithm.NEAREST, (448, 448), (114, 114))
+        attack = Attack(src[0], tar, [scaler_1, scaler_2, scaler_3])
+        att = attack.attack()
+        res_1 = scaler_1.scale_image_with(att, 64, 64)
+        res_2 = scaler_2.scale_image_with(att, 96, 96)
+        res_3 = scaler_3.scale_image_with(att, 114, 114)
+
+        plt.subplot(6, 5, 10 * i + 1)
+        plt.imshow(src[1])
+        plt.title('src 448', fontsize='x-small')
+        plt.axis('off', fontsize='x-small')
+        plt.subplot(6, 5, 10 * i + 2)
+        plt.imshow(att)
+        plt.title('att 448', fontsize='x-small')
+        plt.axis('off')
+        plt.subplot(6, 5, 10 * i + 3)
+        plt.imshow(res_1)
+        plt.title('scale 64', fontsize='x-small')
+        plt.axis('off')
+        plt.subplot(6, 5, 10 * i + 4)
+        plt.imshow(res_2)
+        plt.title('scale 96', fontsize='x-small')
+        plt.axis('off')
+        plt.subplot(6, 5, 10 * i + 5)
+        plt.imshow(res_3)
+        plt.title('scale 114', fontsize='x-small')
+        plt.axis('off')
+
+        scaler_1 = PillowScaler(Algorithm.NEAREST, (1024, 1024), (64, 64))
+        scaler_2 = PillowScaler(Algorithm.NEAREST, (1024, 1024), (96, 96))
+        scaler_3 = PillowScaler(Algorithm.NEAREST, (1024, 1024), (114, 114))
+        attack = Attack(src[1], tar, [scaler_1, scaler_2, scaler_3])
+        att = attack.attack()
         res_1 = scaler_1.scale_image_with(att, 64, 64)
         res_2 = scaler_2.scale_image_with(att, 96, 96)
         res_3 = scaler_3.scale_image_with(att, 114, 114)
         # res_4 = scaler_4.scale_image_with(att, 144, 144)
-        plt.subplot(3, 5, 5*i+1)
-        plt.imshow(src[i])
-        plt.title('src 448')
-        plt.xticks(())
-        plt.yticks(())
-        plt.subplot(3, 5, 5*i+2)
+        plt.subplot(6, 5, 10 * i + 6)
+        plt.imshow(src[1])
+        plt.title('src 1024', fontsize='x-small')
+        plt.axis('off')
+        plt.subplot(6, 5, 10 * i + 7)
         plt.imshow(att)
-        plt.title('att 448')
-        plt.xticks(())
-        plt.yticks(())
-        plt.subplot(3, 5, 5*i+3)
+        plt.title('att 1024', fontsize='x-small')
+        plt.axis('off')
+        plt.subplot(6, 5, 10 * i + 8)
         plt.imshow(res_1)
-        plt.title('scale 64')
-        plt.xticks(())
-        plt.yticks(())
-        plt.subplot(3, 5, 5*i+4)
+        plt.title('scale 64', fontsize='x-small')
+        plt.axis('off')
+        plt.subplot(6, 5, 10 * i + 9)
         plt.imshow(res_2)
-        plt.title('scale 96')
-        plt.xticks(())
-        plt.yticks(())
-        plt.subplot(3, 5, 5*i+5)
+        plt.title('scale 96', fontsize='x-small')
+        plt.axis('off')
+        plt.subplot(6, 5, 10 * i + 10)
         plt.imshow(res_3)
-        plt.title('scale 114')
-        plt.xticks(())
-        plt.yticks(())
-
-        # plt.subplot(166)
-        # plt.imshow(res_4)
-
+        plt.title('scale 114', fontsize='x-small')
+        plt.axis('off')
+    plt.tight_layout()
     plt.show()

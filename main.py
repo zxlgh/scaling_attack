1db77afaebb1a355adaeae2c46217da95008f4e4
wgh <1127003038@qq.com>
2022-11-08 17:51:33 +0800

    use original 1024px image

--- main.py
+++ main.py
@@ -1,15 +1,7 @@
-from torchvision import transforms, datasets
-from PIL import Image
 import matplotlib.pyplot as plt
-import os
-import shutil
-import random
-import numpy as np
 import torch
+import numpy as np
 
-from models import get_model
-from datasets import get_loader
-from train_test import Trainer
 from utils.load_image import load_image_from_disk, load_image_example
 from scale.pillow_scaler import PillowScaler, Algorithm
 from scale.attack import Attack
@@ -17,47 +9,67 @@ from scale.attack import Attack
 
 if __name__ == '__main__':
 
-    src, tar = load_image_from_disk(r'/home/scaling_attack/pub_60/benign/train/0',
-                                    r'/home/scaling_attack/pub_60/benign/train/1',
-                                    r'/home/scaling_attack/pub_60/benign/train/2',
-                                    r'/home/scaling_attack/pub_60/benign/train/3',)
-    scaler_1 = PillowScaler(Algorithm.NEAREST, (224, 224), (64, 64))
-    scaler_2 = PillowScaler(Algorithm.NEAREST, (224, 224), (96, 96))
-    scaler_3 = PillowScaler(Algorithm.NEAREST, (224, 224), (114, 114))
-    for i in range(3):
-        attack = Attack(src[i], tar[i], [scaler_1, scaler_2, scaler_3])
-        att = attack.attack()
-        res_1 = scaler_1.scale_image_with(att, 64, 64)
-        res_2 = scaler_2.scale_image_with(att, 96, 96)
-        res_3 = scaler_3.scale_image_with(att, 114, 114)
-        # res_4 = scaler_4.scale_image_with(att, 144, 144)
-        plt.subplot(3, 5, 5*i+1)
-        plt.imshow(src[i])
-        plt.title('src 224')
-        plt.xticks(())
-        plt.yticks(())
-        plt.subplot(3, 5, 5*i+2)
-        plt.imshow(att)
-        plt.title('att 224')
-        plt.xticks(())
-        plt.yticks(())
-        plt.subplot(3, 5, 5*i+3)
-        plt.imshow(res_1)
-        plt.title('scale 64')
-        plt.xticks(())
-        plt.yticks(())
-        plt.subplot(3, 5, 5*i+4)
-        plt.imshow(res_2)
-        plt.title('scale 96')
-        plt.xticks(())
-        plt.yticks(())
-        plt.subplot(3, 5, 5*i+5)
-        plt.imshow(res_3)
-        plt.title('scale 114')
-        plt.xticks(())
-        plt.yticks(())
+    plt.figure(dpi=300, figsize=(10, 4))
+    src, tar = load_image_from_disk(r'/home/scaling_attack/data/1024',
+                                    r'/home/scaling_attack/data/tar')
+    scaler_1 = PillowScaler(Algorithm.NEAREST, (448, 448), (64, 64))
+    scaler_2 = PillowScaler(Algorithm.NEAREST, (448, 448), (96, 96))
+    scaler_3 = PillowScaler(Algorithm.NEAREST, (448, 448), (114, 114))
+    attack = Attack(src[0], tar, [scaler_1, scaler_2, scaler_3])
+    att = attack.attack()
+    res_1 = scaler_1.scale_image_with(att, 64, 64)
+    res_2 = scaler_2.scale_image_with(att, 96, 96)
+    res_3 = scaler_3.scale_image_with(att, 114, 114)
 
-        # plt.subplot(166)
-        # plt.imshow(res_4)
+    plt.subplot(2, 5, 1)
+    plt.imshow(src[1])
+    plt.title('src 448', fontsize='x-small')
+    plt.axis('off', fontsize='x-small')
+    plt.subplot(2, 5, 2)
+    plt.imshow(att)
+    plt.title('att 448', fontsize='x-small')
+    plt.axis('off')
+    plt.subplot(2, 5, 3)
+    plt.imshow(res_1)
+    plt.title('scale 64', fontsize='x-small')
+    plt.axis('off')
+    plt.subplot(2, 5, 4)
+    plt.imshow(res_2)
+    plt.title('scale 96', fontsize='x-small')
+    plt.axis('off')
+    plt.subplot(2, 5, 5)
+    plt.imshow(res_3)
+    plt.title('scale 114', fontsize='x-small')
+    plt.axis('off')
 
+    scaler_1 = PillowScaler(Algorithm.NEAREST, (1024, 1024), (64, 64))
+    scaler_2 = PillowScaler(Algorithm.NEAREST, (1024, 1024), (96, 96))
+    scaler_3 = PillowScaler(Algorithm.NEAREST, (1024, 1024), (114, 114))
+    attack = Attack(src[1], tar, [scaler_1, scaler_2, scaler_3])
+    att = attack.attack()
+    res_1 = scaler_1.scale_image_with(att, 64, 64)
+    res_2 = scaler_2.scale_image_with(att, 96, 96)
+    res_3 = scaler_3.scale_image_with(att, 114, 114)
+    # res_4 = scaler_4.scale_image_with(att, 144, 144)
+    plt.subplot(2, 5, 6)
+    plt.imshow(src[1])
+    plt.title('src 1024', fontsize='x-small')
+    plt.axis('off')
+    plt.subplot(2, 5, 7)
+    plt.imshow(att)
+    plt.title('att 1024', fontsize='x-small')
+    plt.axis('off')
+    plt.subplot(2, 5, 8)
+    plt.imshow(res_1)
+    plt.title('scale 64', fontsize='x-small')
+    plt.axis('off')
+    plt.subplot(2, 5, 9)
+    plt.imshow(res_2)
+    plt.title('scale 96', fontsize='x-small')
+    plt.axis('off')
+    plt.subplot(2, 5, 10)
+    plt.imshow(res_3)
+    plt.title('scale 114', fontsize='x-small')
+    plt.axis('off')
+    plt.tight_layout()
     plt.show()
--- utils/load_image.py
+++ utils/load_image.py
@@ -10,53 +10,50 @@ def load_image_example():
     :return:
     """
     src = skimage.data.coffee()
-    src = cv.resize(src, (448, 448), interpolation=cv.INTER_CUBIC)
+    src = cv.resize(src, (1024, 1024), interpolation=cv.INTER_CUBIC)
 
     tar_1 = skimage.data.chelsea()
     tar_1 = cv.resize(tar_1, (64, 64), interpolation=cv.INTER_CUBIC)
-
-    tar_2 = skimage.data.astronaut()
+    tar_2 = skimage.data.chelsea()
     tar_2 = cv.resize(tar_2, (96, 96), interpolation=cv.INTER_CUBIC)
-
-    tar_3 = skimage.data.rocket()
+    tar_3 = skimage.data.chelsea()
     tar_3 = cv.resize(tar_3, (114, 114), interpolation=cv.INTER_CUBIC)
 
     return src, [tar_1, tar_2, tar_3]
 
 
-def load_image_from_disk(path_src, path_tar1, path_tar2, path_tar3):
+def load_image_from_disk(path_src, path_tar):
     """
-    # Todo: adding some describing
+
+    :param path_src:
+    :param path_tar:
+    :return:
     """
     files_src = os.listdir(path_src)
-    files_src = files_src[:3]
-    files_tar1 = os.listdir(path_tar1)
-    files_tar1 = files_tar1[:3]
-    files_tar2 = os.listdir(path_tar2)
-    files_tar2 = files_tar2[:3]
-    files_tar3 = os.listdir(path_tar3)
-    files_tar3 = files_tar3[:3]
-    im_src = []
-    im_tar = []
+    files_tar = os.listdir(path_tar)
 
-    for i in range(3):
-        src = cv.imread(os.path.join(path_src, files_src[i]))
-        src = cv.cvtColor(src, cv.COLOR_BGR2RGB)
-        src = cv.resize(src, (224, 224), interpolation=cv.INTER_AREA)
-        im_src.append(src)
-        tar = []
-        im = cv.imread(os.path.join(path_tar1, files_tar1[i]))
-        im = cv.cvtColor(im, cv.COLOR_BGR2RGB)
-        im = cv.resize(im, (64, 64), interpolation=cv.INTER_AREA)
-        tar.append(im)
-        im = cv.imread(os.path.join(path_tar2, files_tar2[i]))
-        im = cv.cvtColor(im, cv.COLOR_BGR2RGB)
-        im = cv.resize(im, (96, 96), interpolation=cv.INTER_AREA)
-        tar.append(im)
-        im = cv.imread(os.path.join(path_tar3, files_tar3[i]))
-        im = cv.cvtColor(im, cv.COLOR_BGR2RGB)
-        im = cv.resize(im, (114, 114), interpolation=cv.INTER_AREA)
-        tar.append(im)
-        im_tar.append(tar)
+    src = []
+    im = cv.imread(os.path.join(path_src, files_src[0]))
+    im = cv.cvtColor(im, cv.COLOR_BGR2RGB)
+    im = cv.resize(im, (448, 448), interpolation=cv.INTER_AREA)
+    src.append(im)
+    im = cv.imread(os.path.join(path_src, files_src[0]))
+    im = cv.cvtColor(im, cv.COLOR_BGR2RGB)
+    # im = cv.resize(im, (1024, 1024), interpolation=cv.INTER_AREA)
+    src.append(im)
 
-    return im_src, im_tar
+    tar = []
+    im = cv.imread(os.path.join(path_tar, files_tar[1]))
+    im = cv.cvtColor(im, cv.COLOR_BGR2RGB)
+    im = cv.resize(im, (64, 64), interpolation=cv.INTER_AREA)
+    tar.append(im)
+    im = cv.imread(os.path.join(path_tar, files_tar[2]))
+    im = cv.cvtColor(im, cv.COLOR_BGR2RGB)
+    im = cv.resize(im, (96, 96), interpolation=cv.INTER_AREA)
+    tar.append(im)
+    im = cv.imread(os.path.join(path_tar, files_tar[3]))
+    im = cv.cvtColor(im, cv.COLOR_BGR2RGB)
+    im = cv.resize(im, (114, 114), interpolation=cv.INTER_AREA)
+    tar.append(im)
+
+    return src, tar

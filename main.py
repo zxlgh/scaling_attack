a27c81ae7cf673371a12e5d4c5ee6ee69585ddf0
wgh <1127003038@qq.com>
2022-11-10 16:44:10 +0800

    saving every image to adjust conveniently eventual layout.

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import matplotlib.pyplot as plt
 import torch
 import numpy as np
+import os
+from PIL import Image
 
 from utils.load_image import load_image_from_disk, load_image_example
 from scale.pillow_scaler import PillowScaler, Algorithm
@@ -9,9 +11,11 @@ from scale.attack import Attack
 
 if __name__ == '__main__':
 
-    fig = plt.figure(dpi=300, figsize=(6, 3))
+    path = r'/home/scaling_attack/image/diff-size/448'
+    if not os.path.exists(path):
+        os.makedirs(path)
 
-    img = load_image_from_disk(r'/home/scaling_attack/data/animal')
+    img = load_image_from_disk(r'/home/scaling_attack/data/landscape')
     scaler_1 = PillowScaler(Algorithm.NEAREST, (448, 448), (64, 64))
     scaler_2 = PillowScaler(Algorithm.NEAREST, (448, 448), (96, 96))
     scaler_3 = PillowScaler(Algorithm.NEAREST, (448, 448), (114, 114))
@@ -20,28 +24,20 @@ if __name__ == '__main__':
     res_1 = scaler_1.scale_image_with(att, 64, 64)
     res_2 = scaler_2.scale_image_with(att, 96, 96)
     res_3 = scaler_3.scale_image_with(att, 114, 114)
+    im = Image.fromarray(img[0])
+    im.save(os.path.join(path, 'src_448.eps'))
+    im = Image.fromarray(att)
+    im.save(os.path.join(path, 'att_448.eps'))
+    im = Image.fromarray(res_1)
+    im.save(os.path.join(path, 'scale_64.eps'))
+    im = Image.fromarray(res_2)
+    im.save(os.path.join(path, 'scale_96.eps'))
+    im = Image.fromarray(res_3)
+    im.save(os.path.join(path, 'scale_114.eps'))
 
-    plt.subplot(2, 5, 1)
-    plt.imshow(img[1])
-    plt.title('src 448', fontsize='x-small')
-    plt.axis('off')
-    plt.subplot(2, 5, 2)
-    plt.imshow(att)
-    plt.title('att 448', fontsize='x-small')
-    plt.axis('off')
-    plt.subplot(2, 5, 3)
-    plt.imshow(res_1)
-    plt.title('scale 64', fontsize='x-small')
-    plt.axis('off')
-    plt.subplot(2, 5, 4)
-    plt.imshow(res_2)
-    plt.title('scale 96', fontsize='x-small')
-    plt.axis('off')
-    plt.subplot(2, 5, 5)
-    plt.imshow(res_3)
-    plt.title('scale 114', fontsize='x-small')
-    plt.axis('off')
-
+    path = r'/home/scaling_attack/image/diff-size/1024'
+    if not os.path.exists(path):
+        os.makedirs(path)
     scaler_1 = PillowScaler(Algorithm.NEAREST, (1024, 1024), (64, 64))
     scaler_2 = PillowScaler(Algorithm.NEAREST, (1024, 1024), (96, 96))
     scaler_3 = PillowScaler(Algorithm.NEAREST, (1024, 1024), (114, 114))
@@ -50,26 +46,13 @@ if __name__ == '__main__':
     res_1 = scaler_1.scale_image_with(att, 64, 64)
     res_2 = scaler_2.scale_image_with(att, 96, 96)
     res_3 = scaler_3.scale_image_with(att, 114, 114)
-    plt.subplot(2, 5, 6)
-    plt.imshow(img[1])
-    plt.title('src 1024', fontsize='x-small')
-    plt.axis('off')
-    plt.subplot(2, 5, 7)
-    plt.imshow(att)
-    plt.title('att 1024', fontsize='x-small')
-    plt.axis('off')
-    plt.subplot(2, 5, 8)
-    plt.imshow(res_1)
-    plt.title('scale 64', fontsize='x-small')
-    plt.axis('off')
-    plt.subplot(2, 5, 9)
-    plt.imshow(res_2)
-    plt.title('scale 96', fontsize='x-small')
-    plt.axis('off')
-    plt.subplot(2, 5, 10)
-    plt.imshow(res_3)
-    plt.title('scale 114', fontsize='x-small')
-    plt.axis('off')
-    fig.subplots_adjust(top=0.95, bottom=0.05, left=0.05, right=0.95, wspace=0.1, hspace=0.1)
-    plt.savefig('3-size.eps')
-    plt.show()
+    im = Image.fromarray(img[1])
+    im.save(os.path.join(path, 'src_1024.eps'))
+    im = Image.fromarray(att)
+    im.save(os.path.join(path, 'att_1024.eps'))
+    im = Image.fromarray(res_1)
+    im.save(os.path.join(path, 'scale_64.eps'))
+    im = Image.fromarray(res_2)
+    im.save(os.path.join(path, 'scale_96.eps'))
+    im = Image.fromarray(res_3)
+    im.save(os.path.join(path, 'scale_114.eps'))

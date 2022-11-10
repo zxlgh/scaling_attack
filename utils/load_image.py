205ec5df20729a3c3bff54ec8eaac58642d5dc31
wgh <1127003038@qq.com>
2022-11-10 10:48:47 +0800

    adjust the figure size

--- utils/load_image.py
+++ utils/load_image.py
@@ -22,38 +22,28 @@ def load_image_example():
     return src, [tar_1, tar_2, tar_3]
 
 
-def load_image_from_disk(path_src, path_tar):
+def load_image_from_disk(path):
     """
 
-    :param path_src:
-    :param path_tar:
-    :return:
     """
-    files_src = os.listdir(path_src)
-    files_tar = os.listdir(path_tar)
+    files = os.listdir(path)
 
-    src = []
-    im = cv.imread(os.path.join(path_src, files_src[0]))
-    im = cv.cvtColor(im, cv.COLOR_BGR2RGB)
+    img = []
+    im = cv.imread(os.path.join(path, files[0]))
     im = cv.resize(im, (448, 448), interpolation=cv.INTER_AREA)
-    src.append(im)
-    im = cv.imread(os.path.join(path_src, files_src[0]))
-    im = cv.cvtColor(im, cv.COLOR_BGR2RGB)
-    # im = cv.resize(im, (1024, 1024), interpolation=cv.INTER_AREA)
-    src.append(im)
+    img.append(im)
+    im = cv.imread(os.path.join(path, files[0]))
+    # im = cv.resize(im, (1024, 448), interpolation=cv.INTER_AREA)
+    img.append(im)
 
-    tar = []
-    im = cv.imread(os.path.join(path_tar, files_tar[1]))
-    im = cv.cvtColor(im, cv.COLOR_BGR2RGB)
+    im = cv.imread(os.path.join(path, files[1]))
     im = cv.resize(im, (64, 64), interpolation=cv.INTER_AREA)
-    tar.append(im)
-    im = cv.imread(os.path.join(path_tar, files_tar[2]))
-    im = cv.cvtColor(im, cv.COLOR_BGR2RGB)
+    img.append(im)
+    im = cv.imread(os.path.join(path, files[2]))
     im = cv.resize(im, (96, 96), interpolation=cv.INTER_AREA)
-    tar.append(im)
-    im = cv.imread(os.path.join(path_tar, files_tar[3]))
-    im = cv.cvtColor(im, cv.COLOR_BGR2RGB)
+    img.append(im)
+    im = cv.imread(os.path.join(path, files[3]))
     im = cv.resize(im, (114, 114), interpolation=cv.INTER_AREA)
-    tar.append(im)
+    img.append(im)
 
-    return src, tar
+    return img

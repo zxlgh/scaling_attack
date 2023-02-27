328b79e8774ad0e9c67071db8a67a40487e5a8d2
wgh <wgh@njust.edu.cn>
2023-02-27 17:14:07 +0800

    optimization

--- main.py
+++ main.py
@@ -11,13 +11,24 @@ from scale.attack import Attack
 
 if __name__ == '__main__':
 
-    src, t1, t2, t3 = load_image_from_disk(r'/home/data/face')
+    src, t1, t2, t3 = load_image_from_disk(r'/home/data/landscape')
     s1 = PillowScaler(Algorithm.NEAREST, (1024, 1024), (64, 64))
     s2 = PillowScaler(Algorithm.NEAREST, (1024, 1024), (96, 96))
     s3 = PillowScaler(Algorithm.NEAREST, (1024, 1024), (112, 112))
     attack = Attack(src, [t1], [s1])
     att = attack.attack()
+    res1 = s1.scale_image_with(att, 64, 64)
+    res2 = s2.scale_image_with(att, 96, 96)
+    res3 = s3.scale_image_with(att, 112, 112)
 
+    plt.subplot(151)
+    plt.imshow(src)
+    plt.subplot(152)
+    plt.imshow(att)
+    plt.subplot(153)
+    plt.imshow(res1)
+
+    plt.show()
     print(ssim(src, att))
     print(msssim(src, att))
     print(uqi(src, att))
